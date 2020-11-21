d1c8682e7da7ba504043316248a69d59f3b6346c
José M. Viñas <josemanuelvinhascid@gmail.com>
2020-11-21 22:38:05 +0100

    Follow implementado

--- rest/FollowerRest.php
+++ rest/FollowerRest.php
@@ -1,7 +1,51 @@
 <?php
 
+require_once(__DIR__ . "/BaseRest.php");
 
-class ProfileRest
+require_once(__DIR__ . "/../model/Follower.php");
+require_once(__DIR__ . "/../model/FollowerMapper.php");
+
+class FollowerRest extends BaseRest
 {
+    private $followerMapper;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->followerMapper = new FollowerMapper();
+    }
+
+    public function follow($username)
+    {
+        $currentLogged = parent::authenticateUser();
+
+        $follower = new Follower($currentLogged->getUsername(), $username);
+
+        if ($this->followerMapper->isFollowing($follower->getUsernameFollower(), $follower->getUsernameFollowing())) {
+            http_response_code(400);
+            header('Content-Type: application/json');
+            echo(json_encode(array("follower" => "Already follow this user")));
+        } elseif ($follower->getUsernameFollower() == $follower->getUsernameFollowing()) {
+            http_response_code(400);
+            header('Content-Type: application/json');
+            echo(json_encode(array("follower" => "A user cannot follow himself")));
+        } else {
+            $this->followerMapper->save($follower);
+            header($_SERVER['SERVER_PROTOCOL'] . ' 201 Created');
+        }
+
+
+    }
+
+    public function unfollow()
+    {
+        //TODO
+    }
 
 }
+
+$followerRest = new FollowerRest();
+URIDispatcher::getInstance()
+    ->map("POST", "/follower/$1", array($followerRest, "follow"))
+    ->map("DELETE", "/follower/$1", array($followerRest, "unfollow"));
