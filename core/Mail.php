f0b15349a45d08b4de0752b68bc6af6c6449e48b
José M. Viñas <josemanuelvinhascid@gmail.com>
2021-01-20 19:03:05 +0100

    Envio de mails
    Signed-off-by: José M. Viñas <josemanuelvinhascid@gmail.com>

--- core/Mail.php
+++ core/Mail.php
@@ -1,7 +1,52 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require '../vendor/autoload.php';
 
 class Mail
 {
 
+    private PHPMailer $mail;
+
+    public function __construct($address, $name, $subject, $body)
+    {
+        $this->mail = new PHPMailer();
+
+        $this->mail->isSMTP();
+
+        $this->mail->SMTPDebug = SMTP::DEBUG_OFF;
+
+        $this->mail->Host = 'smtp.gmail.com';
+
+        $this->mail->Port = 587;
+
+        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        $this->mail->SMTPAuth = true;
+
+        $this->mail->Username = 'tiktak.info.noreply@gmail.com';
+
+        $this->mail->Password = 'tiktakpass1234';
+
+        $this->mail->setFrom('tiktak.info.noreply@gmail.com', 'TikTak');
+
+        $this->mail->addAddress($address, $name);
+
+        $this->mail->Subject = $subject;
+
+        $this->mail->Body= $body;
+
+
+    }
+
+    public function sendMail()
+    {
+        try {
+            $this->mail->send();
+        } catch (\PHPMailer\PHPMailer\Exception $e) {
+
+        }
+    }
 }
